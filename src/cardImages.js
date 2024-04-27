dd8227fdfb15df32c3b9f3002c735bc5399e177c
Denzel <dw@DESKTOP-POCROHT>
2024-04-27 17:29:39 -0300

    Add data cardImages

---
+++ src/cardImages.js
@@ -0,0 +1,15 @@
+import chat from '../public/img/chat.png'
+import img from '../public/img/img.png'
+import target from '../public/img/target.png'
+import internet from '../public/img/internet.png'
+import king from '../public/img/king.png'
+import money from '../public/img/money.png'
+
+export const cardImages = [
+    {"src": chat, "matched": false},
+    {"src": img, "matched": false},
+    {"src": target, "matched": false},
+    {"src": internet, "matched": false},
+    {"src": king, "matched": false},
+    {"src": money, "matched": false},
+]
